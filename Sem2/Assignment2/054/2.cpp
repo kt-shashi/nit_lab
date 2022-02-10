c56139a137487427862c96d0b4e14a4445fdb4dd
SHUBHANSHU SINGH <90331406+soshubh@users.noreply.github.com>
2022-02-10 15:13:45 +0530

    Update 2.cpp

--- Sem2/Assignment2/054/2.cpp
+++ Sem2/Assignment2/054/2.cpp
@@ -3,48 +3,54 @@ using namespace std;
 #define N 100
 void print_row_major(int array[N][N], int no_row, int no_column)
 {
-    cout << "\nRow major representation-\n";
-    for (int row = 0; row < no_row; row++)
-        for (int column = 0; column < no_column; column++)
-            cout << array[row][column] << " ";
+  cout << "\nRow major representation-\n";
+  for (int row = 0; row < no_row; row++)
+  {
+    for (int column = 0; column < no_column; column++)
+      cout << array[row][column] << " ";
+    cout << "\n";
+  }
 }
 void print_column_major(int array[N][N], int no_row, int no_column)
 {
-    cout << "\nColumn major representation-\n";
+  cout << "\nColumn major representation-\n";
 
-    for (int column = 0; column < no_column; column++)
-        for (int row = 0; row < no_row; row++)
-            cout << array[row][column] << " ";
+  for (int column = 0; column < no_column; column++)
+  {
+    for (int row = 0; row < no_row; row++)
+      cout << array[row][column] << " ";
+    cout << "\n";
+  }
 }
-void find_order(int array[N][N],int row,int column)
+void find_order(int array[N][N], int row, int column)
 {
-    int find_row, find_column;
+  int find_row, find_column;
 
-    cout << "\nEnter the indexe for find order's : ";    
-    cin >> find_row >> find_column;
+  cout << "\nEnter the indexe for find order's : ";
+  cin >> find_row >> find_column;
 
-    cout<<array[find_row-1][find_column-1]<<"\n";
-    cout << "Row Major order : " << (find_row - 1) * sizeof(int) + find_column << "\n";
-    cout << "Column Major order : " << (find_column - 1) * sizeof(int) + find_row;
+  cout << array[find_row - 1][find_column - 1] << "\n";
+  cout << "Row Major order : " << (find_row - 1) * row + find_column << "\n";
+  cout << "Column Major order : " << (find_column - 1) * column + find_row;
 }
 int main()
 {
-    int r, c;
-    cout << "Enter number of row : ";
-    cin >> r;
-    cout << "Enter number of column : ";
-    cin >> c;
-    int array[N][N];
-    cout << "Enter Matrix elements : \n";
-    for (int row = 0; row < r; row++)
-        for (int column = 0; column < c; column++)
-            cin >> array[row][column];
-    cout << "\n-------------------------------";
-    print_row_major(array, r, c);
-    cout << "\n-------------------------------";
-    print_column_major(array, r, c);
-    cout << "\n-------------------------------";
-    find_order(array,r,c);
-    cout << "\n-------------------------------";
-    return 0;
+  int r, c;
+  cout << "Enter number of row : ";
+  cin >> r;
+  cout << "Enter number of column : ";
+  cin >> c;
+  int array[N][N];
+  cout << "Enter Matrix elements : \n";
+  for (int row = 0; row < r; row++)
+    for (int column = 0; column < c; column++)
+      cin >> array[row][column];
+  cout << "\n-------------------------------";
+  print_row_major(array, r, c);
+  cout << "\n-------------------------------";
+  print_column_major(array, r, c);
+  cout << "\n-------------------------------";
+  find_order(array, r, c);
+  cout << "\n-------------------------------";
+  return 0;
 }
