6a4edbc0d47c7118ffcabb139bca8e501071164a
SHUBHANSHU SINGH <90331406+soshubh@users.noreply.github.com>
2022-02-17 18:33:03 +0530

    Update 1.cpp

--- Sem2/Assignment3/54/1.cpp
+++ Sem2/Assignment3/54/1.cpp
@@ -1,32 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define max 100
+
 void push(string str, int top, char stack[], int i)
 {
-      if (top == max - 1)
-        cout << "We don't have that much space!";
-      else
-        stack[top] = str[i];
+  if (top == max - 1)
+    cout << "We don't have that much space!";
+  else
+    stack[top] = str[i];
 }
 
 void pop(int top, char stack[], string &str)
 {
-      str[top] = stack[top];
+  str[top] = stack[top];
 }
+
 int main()
 {
-      string str;
-      cout << "Enter your string : ";
-      getline(cin, str);
-      char stack[max];
-      int top = 0;
+  string str;
+  cout << "----------------------------\n";
+  cout << "Enter your string : ";
+  getline(cin, str);
+  char stack[max];
+  int top = 0;
 
-      for (int i = str.length() - 1; i >= 0; i--)
-            push(str, top++, stack, i);
+  for (int i = str.length() - 1; i >= 0; i--)
+    push(str, top++, stack, i);
 
-      for (int i = 0; i < str.length(); i++)
-            pop(--top, stack, str);
+  for (int i = 0; i < str.length(); i++)
+    pop(--top, stack, str);
 
-      cout << "Reversed string : " << str;
+  cout << "----------------------------\n";
+  cout << "Reversed string : " << str;
+  cout << "\n----------------------------";
+
   return 0;
 }
