4396ab25c9468723b6bda061f4b3c3ae9a805bb2
Shashi Kant <shashiofficial10@gmail.com>
2022-02-24 22:53:47 +0530

    Added A1 Q2

---
+++ Sem2/cplusplus/Assignment1/50/2.cpp
@@ -0,0 +1,80 @@
+// Assignment 1 Program 2
+// Name: Shashi Kant
+// Roll: 2021PGCACA050
+
+#include <iostream>
+#include <iomanip>
+#define MAXSIZE 2
+#define SGST 9.0
+#define CGST 9.0
+using namespace std;
+
+struct Product
+{
+    int productId;
+    string productName;
+    double currentDiscountPercentage;
+    double productLabelPrice;
+    double userRating;
+};
+
+// Input Kyc data
+void input_detail(struct Product &product)
+{
+
+    cout << endl
+         << "Enter product details: " << endl
+         << endl;
+    cout << "Enter Product Id: ";
+    cin >> product.productId;
+    cout << "Enter Product Name: " << endl;
+    cin >> product.productName;
+    cout << "Enter Current Discount Percentage: ";
+    cin >> product.currentDiscountPercentage;
+    cout << "Enter Product Label Price: ";
+    cin >> product.productLabelPrice;
+    cout << "Enter User Rating: ";
+    cin >> product.userRating;
+}
+
+// Output Kyc data
+void output_detail(struct Product &product)
+{
+    cout << endl
+         << "Vewing product details" << endl
+         << endl;
+    cout << "Product Id: " << product.productId << endl;
+    cout << "Product Name: " << product.productName << endl;
+    cout << "Current Discount Percentage: " << fixed << setprecision(2) << product.currentDiscountPercentage << "%" << endl;
+    cout << "Product Label Price: Rs." << fixed << setprecision(2) << product.productLabelPrice << endl;
+    cout << "User Rating: " << fixed << setprecision(2) << product.userRating << endl;
+}
+
+// Find sales price
+double computesalesprice(struct Product &product)
+{
+
+    double discount = product.productLabelPrice - ((product.currentDiscountPercentage / 100) * product.productLabelPrice);
+
+    cout << endl
+         << "Disc: " << discount << endl;
+    cout << "sgst: " << (product.productLabelPrice * (SGST / 100)) << endl;
+    cout << "cgst: " << (product.productLabelPrice * (CGST / 100)) << endl;
+
+    return (product.productLabelPrice - discount + (product.productLabelPrice * (SGST / 100)) + (product.productLabelPrice * (CGST / 100)));
+}
+
+int main()
+{
+
+    int n = MAXSIZE;
+    struct Product products[n];
+
+    // Loop for user input
+    for (int i = 0; i < n; i++)
+    {
+        input_detail(products[i]);
+        output_detail(products[i]);
+        cout << "The sales price of a product is Rs." << fixed << setprecision(2) << computesalesprice(products[i]) << endl;
+    }
+}
