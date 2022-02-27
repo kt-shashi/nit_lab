6c3f0859b22a54b5434b3102c297a476d73be07b
Shashi Kant <shashiofficial10@gmail.com>
2022-02-27 19:04:21 +0530

    Added A1 Q8

---
+++ Sem2/cplusplus/Assignment1/50/8.cpp
@@ -0,0 +1,38 @@
+// Assignment 1 Program 8
+// Name: Shashi Kant
+// Roll: 2021PGCACA050
+
+#define __STDCPP_WANT_MATH_SPEC_FUNCS__ 1
+
+#include <iostream>
+#include <math.h>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+
+    double inputG;
+    // Gamma function
+    cout << "Enter number to find value if it's Gamma function: ";
+    cin >> inputG;
+
+    cout << "Gamma function value for " << inputG << " is: " << tgamma(inputG) << endl;
+
+    double inputB1, inputB2;
+    // Beta function
+    cout << "Enter the first value for Beta function: ";
+    cin >> inputB1;
+    cout << "Enter the second value for Beta function: ";
+    cin >> inputB2;
+
+    cout << "Result of Beta function for " << inputB1 << " and " << inputB2 << " is: " << beta(inputB1, inputB2) << "\n";
+
+    // // Psi function
+    // double inputPSI;
+    // cout << "Enter number to find value if it's Psi function: ";
+    // cin >> inputPSI;
+
+    // cout << "Psi function value for " << inputPSI << " is: " << psi(inputPSI) << endl;
+}
