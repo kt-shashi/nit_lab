3a603853abf15ab7a49c94307e83edf3060d17ab
Shashi Kant <shashiofficial10@gmail.com>
2022-04-23 19:59:14 +0530

    Updated something idk

--- Sem2/cplusplus/Assignment8/1.cpp
+++ Sem2/cplusplus/Assignment8/1.cpp
@@ -3,50 +3,50 @@
 // 2021pgcaca050
 
 #include <iostream>
-#include <algorithm>
 #include <climits>
 
 using namespace std;
 
-#include <iostream>
-using namespace std;
-
-void test(int a, int b)
+void checkForError(int x)
 {
-
     try
     {
-        if (b == 0)
-            throw b;
-        else if (b < 0)
-            throw 'b';
-        else
-            throw(double) b;
+        if (x < 0)
+            throw x;
+        else if (x > INT_MAX)
+            throw 'x';
+        else if (x == 0)
+            throw 1.4f;
     }
-    catch (signed int i)
+    catch (int x)
     {
-        cout << "Caught exception : denominator equal to zero \n";
+        cout << "Cannot divide by negative number";
     }
-    catch (char c)
+    catch (char x)
     {
-        cout << "Caught exception : negative number \n";
+        cout << "Divisor cannot be this large";
     }
-    catch (double d)
+    catch (float x)
     {
-        cout << "Caught exception : long long type \n";
+        cout << "Divisor cannot be zero";
     }
 }
+
 int main()
 {
 
-    int a, b;
-    cout << "\nEnter the value of numerator and denominator,respectively : ";
-    cin >> a >> b;
-    cout << "\n\nUser Defined exception Handling : \n";
-    test(a, b);
-    cout << "\n\nTesting multiple catches\n";
-    test(a, 0);
-    test(a, -10);
-    test(a, INT_MAX);
+    int a, c;
+    long b;
+
+    cout << "Enter divident: ";
+    cin >> a;
+    cout << "Enter divisor: ";
+    cin >> b;
+
+    checkForError(b);
+
+    c = a / b;
+    cout << c;
+
     return 0;
 }
